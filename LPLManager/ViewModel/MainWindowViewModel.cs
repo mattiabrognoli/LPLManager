993adb6f0ca9189a29a7d8c66752a9c3e4d009c5
mattiabrognoli <mattia.brognoli9@gmail.com>
2021-06-01 20:20:15 +0200

    Inizializzato MainWindowViewModel

---
+++ LPLManager/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPLManager.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        string _selectedChoice;
+
+
+        public string SelectedChoice
+        {
+            get
+            {
+                return _selectedChoice;
+            }
+            set
+            {
+                _selectedChoice = value;
+                OnPropertyChanged(nameof(SelectedChoice));
+            }
+        }
+
+
+        public MainWindowViewModel()
+        {
+        }
+    }
+}
---
+++ LPLManager/ViewModel/ViewModelBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPLManager.ViewModel
+{
+    public class ViewModelBase
+    {
+        // In ViewModelBase.cs
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (VerifyPropertyName(propertyName))
+            {
+                PropertyChangedEventHandler handler = this.PropertyChanged;
+                if (handler != null)
+                {
+                    var e = new PropertyChangedEventArgs(propertyName);
+                    handler(this, e);
+                }
+            }
+        }
+
+        public bool VerifyPropertyName(string propertyName)
+        {
+            // Verify that the property name matches a real,  
+            // public, instance property on this object.
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+            {
+                //string msg = "Invalid property name: " + propertyName;
+
+                //if (this.ThrowOnInvalidPropertyName)
+                //    throw new Exception(msg);
+                //else
+                //    Debug.Fail(msg);
+                return false;
+            }
+            return true;
+        }
+    }
+}
